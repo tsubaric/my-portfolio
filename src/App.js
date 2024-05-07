dfcf88dbb8ae61acf25f998c71d1be2b5193df64
Tommy Subaric <91640793+tsubaric@users.noreply.github.com>
2024-05-07 11:58:52 -0500

    basic clean up changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="mint-green-bar">
+        <h1>Title</h1>
+        <img src={logo} alt="Logo" />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
