9a33bbedb50d9f64d5a21460c8716fee7b44c586
Tommy Subaric <91640793+tsubaric@users.noreply.github.com>
2024-05-21 12:21:17 -0500

    adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function App() {
   return (
@@ -10,19 +18,19 @@ function App() {
         <div className="social-media-buttons">
           <h4>Test Subheader</h4>
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-            <img src="facebook-icon.png" alt="Facebook" />
+            <FacebookIcon fontSize="medium" alt="Facebook"/>
           </a>
           <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer">
-            <img src="twitter-icon.png" alt="Twitter" />
+            <TwitterIcon fontSize="medium" alt="Twitter"/>
           </a>
           <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-            <img src="instagram-icon.png" alt="Instagram" />
+            <InstagramIcon fontSize="medium" alt="Instagram"/>
           </a>
           <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-            <img src="linkedin-icon.png" alt="LinkedIn" />
+            <LinkedInIcon fontSize="medium" alt="LinkedIn"/>
           </a>
           <a href="https://www.github.com/" target="_blank" rel="noopener noreferrer">
-            <img src="github-icon.png" alt="GitHub" />
+            <GitHubIcon fontSize="medium" alt="GitHub"/>
           </a>
         </div>
         <div className="separator"></div> {/* Faded line separator */}
@@ -37,6 +45,10 @@ function App() {
           <p>
             This is a description. Add more details about the person, their role, and any other relevant information here.
           </p>
+          <div className="button-group">
+            <Button variant="contained" color="primary">Button 1</Button>
+            <Button variant="contained" color="secondary">Button 2</Button>
+          </div>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
