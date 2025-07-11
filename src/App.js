7194cebf8af80ce458cde4a6bcf83364284f23f7
INCITServicePWR <INC_ITServicePWRApps@hydraforce.com>
2025-07-11 15:19:02 -0500

    go

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import projects from './projects.json';
 import LinkIcon from '@mui/icons-material/Link';
