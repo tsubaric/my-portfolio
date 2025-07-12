2703f56df9ca0cd153ee69a0a763cd69a6a131b3
Tommy Subaric <91640793+tsubaric@users.noreply.github.com>
2025-07-12 15:31:44 -0500

    go

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import portrait from './images/Arthur Morgan.jpeg';
+import portrait from './images/ArthurMorgan.jpeg';
 import './App.css';
 import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
