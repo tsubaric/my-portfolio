a6224b504188dfc67d45e952595f9454ce05a391
Tommy Subaric <91640793+tsubaric@users.noreply.github.com>
2026-04-10 11:21:38 -0500

    card media removed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+// import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import projects from './projects.json';
 import LinkIcon from '@mui/icons-material/Link';
